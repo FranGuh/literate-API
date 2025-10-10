e7583b2bbefed05f02c5b0beac48824485cd595e
FranGuh <pedicles62900@gmail.com>
2025-10-10 12:00:01 -0600

    Arreglado bug en produccion, port inaccesible

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'test') {
   require('dotenv').config();
 }
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`Servidor en el puerto ${port}`);
